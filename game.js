507641bda98d0ae87344afaee47ea2438da8aca7
guido2288 <guido2288@gmail.com>
2023-02-12 16:23:05 -0300

    Render terminado

---
+++ game.js
@@ -0,0 +1,50 @@
+const canvas = document.querySelector('#game');
+const game = canvas.getContext('2d');
+
+let canvasSize;
+let elementSize;
+
+window.addEventListener('load', setCanvasSize);
+window.addEventListener('resize', setCanvasSize);
+
+
+
+
+function setCanvasSize() {
+
+  if (window.innerHeight > window.innerWidth) {
+    canvasSize = window.innerWidth * 0.8;
+  } else {
+    canvasSize = window.innerHeight * 0.8;
+  }
+
+  canvas.setAttribute('width', canvasSize);
+  canvas.setAttribute('height', canvasSize);
+
+  elementSize = canvasSize / 10;
+
+  startGame();
+
+};
+
+function startGame() {
+
+  game.font = elementSize + 'px Verdana';
+  game.textAlign = 'end';
+
+  const map = maps[0];
+  const mapsRows = map.trim().split('\n');
+  const mapRowCols = mapsRows.map(row => row.trim().split(''));
+
+  mapRowCols.forEach((row, rowIndex) => {
+    row.forEach((col, colIndex) => {
+      const emoji = emojis[col];
+      const postX = elementSize * (colIndex + 1);
+      const postY = elementSize * (rowIndex + 1);
+
+      game.fillText(emoji, postX, postY)
+
+    });
+  });
+
+};
